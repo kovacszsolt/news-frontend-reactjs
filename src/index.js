d3420a85ba6802d0ba85a24400130dc49e1f986c
kovacs zsolt <smith.zsolt@gmail.com>
2019-03-28 14:48:23 +0100

    new build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(process.env);
+document.getElementsByTagName("head")[0].insertAdjacentHTML(
+    "afterbegin",
+    "<link rel=\"stylesheet\" href=\"/" + process.env.REACT_APP_SCHEMA_CSS + "\" />");
+document.getElementsByTagName("head")[0].insertAdjacentHTML(
+    "afterbegin",
+    "<link rel=\"stylesheet\" href=\"/colors.css\" />");
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
