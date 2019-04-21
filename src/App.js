6008f2969b71e62607f481df4510d3167a30f318
Kovács Zsolt <smith.zsolt@gmail.com>
2019-04-21 23:27:09 +0200

    WIP new state

--- src/App.js
+++ src/App.js
@@ -12,125 +12,127 @@ import ServicesRemote from "./services/Services.remote";
 import ServicesIndexedDB from "./services/Services.indexeddb";
 
 class App extends Component {
-  db;
+    db;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      privacy: document.cookie.split(';').find(cookie => cookie.trim() === 'privacy=allow') === undefined ? false : true
-    };
-    this.init();
-  }
-
-  initIndexedDB = () => {
-    ServicesIndexedDB.createDatabase().then((db) => {
-      this.db = db;
-      ServicesIndexedDB.getRecordount(db).then((recordCount) => {
-        const updateKey = localStorage.getItem(process.env.REACT_APP_UPDATEKEY);
-        ServicesRemote.getUpdate().then((updateRecords) => {
-          if (String(updateRecords.lastAddDate) !== updateKey) {
-            console.log('db update');
-            this.initIndexedDBData();
-            localStorage.setItem(process.env.REACT_APP_UPDATEKEY, updateRecords.lastAddDate);
-          } else {
-            console.log('db no update');
-            this.setState({isLoading: false});
-          }
-        });
-      });
-    });
-  };
-
-  initIndexedDBData = () => {
-    ServicesRemote.getAll().then((records) => {
-      const transList = this.db.transaction('news', 'readwrite');
-      const storeObject = transList.objectStore('news');
-      const transTagList = this.db.transaction('tags', 'readwrite');
-      const storeTagObject = transTagList.objectStore('tags');
-      records.map(record => record.slug = (record.meta === undefined ? '' : record.meta.slug));
-      records = records.filter(record => record.slug !== '');
-      records.map(record => record.title = record.meta.title);
-      records.map(record => record.description = record.meta.description);
-      records.map(record => record.extension = record.meta.extension);
-      records.map(record => record.createtime = record.meta.createtime);
-
-      const tagRecords = [];
-      records.forEach((record) => {
-        record.tags.forEach((tag) => {
-          const _record = {...record};
-          _record.tag = tag;
-          tagRecords.push(_record);
-        });
-      });
-      Promise.all(records.map(record => ServicesIndexedDB.insertRecord(storeObject, record))).then(() => {
-        this.setState({isLoading: false});
-      });
-      Promise.all(tagRecords.map(tagRecord => ServicesIndexedDB.insertRecord(storeTagObject, tagRecord))).then(() => {
-        this.setState({isLoading: false});
-      });
-    });
-  }
-
-  init() {
-    this.initIndexedDB();
-  }
-
-  addCookie = () => {
-    const _date = new Date();
-    _date.setFullYear(_date.getFullYear() + 1);
-    document.cookie = "privacy=allow; expires=" + _date.toGMTString();
-    this.setState({privacy: true});
-  }
-
-  render() {
-    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY);
-    ReactGA.pageview(window.location.pathname + window.location.search);
-    if (this.state.isLoading) {
-      return (
-          <div className="app__center">
-            <span className="fas fa-spinner fa-spin"></span>
-          </div>
-      )
-    } else {
-      return (
-          <Fragment>
-            {(!this.state.privacy) ?
-                <div className="app__overlay">
-                  <div className="app__overlay__popup">
-                    <h2>Fontos</h2>
-                    <div className="app__overlay__popup__content">
-                      Az oldal cookie-kat használ a jobb működés érdekében!
-                    </div>
-                    <div className="app__overlay__popup__footer">
-                      <button onClick={this.addCookie}>Rendben</button>
-                    </div>
-                  </div>
-                </div>
-                : ''}
-            <Router>
-              <Fragment>
-                <AppCommonHeader/>
-                <div className="w-100 p-3">
-                  <Switch>
-                    <Route path="/" exact component={AppFront}/>
-                    <Route path="/notfound" exact component={AppNotfound}/>
-                    <Route path="/search" exact component={AppSearch}/>
-                    <Route path="/search/:slug" exact component={AppSearch}/>
-                    <Route path="/:slug" exact component={AppTweet}/>
-                    <Route path="/tag/:slug" exact component={AppTag}/>
-                  </Switch>
-                </div>
-              </Fragment>
-            </Router>
-          </Fragment>
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            privacy: document.cookie.split(';').find(cookie => cookie.trim() === 'privacy=allow') === undefined ? false : true
+        };
+        this.init();
     }
-  }
+
+    initIndexedDB = () => {
+        ServicesIndexedDB.createDatabase().then((db) => {
+            this.db = db;
+            ServicesIndexedDB.getRecordount(db).then((recordCount) => {
+                const updateKey = localStorage.getItem(process.env.REACT_APP_UPDATEKEY);
+                ServicesRemote.getUpdate().then((updateRecords) => {
+                    console.log('updateRecords', updateRecords);
+                    console.log('updateKey', updateKey);
+                    if ((updateKey === null) || (updateRecords['lastAddDate'] !== updateKey['lastAddDate'])) {
+
+                        console.log('db update');
+                        this.initIndexedDBData();
+                        localStorage.setItem(process.env.REACT_APP_UPDATEKEY, updateRecords);
+                    } else {
+                        console.log('db no update');
+                        this.setState({isLoading: false});
+                    }
+                });
+            });
+        });
+    };
+
+    initIndexedDBData = () => {
+        ServicesRemote.getAll().then((records) => {
+            const transList = this.db.transaction('news', 'readwrite');
+            const storeObject = transList.objectStore('news');
+            const transTagList = this.db.transaction('tags', 'readwrite');
+            const storeTagObject = transTagList.objectStore('tags');
+            records.map(record => record.slug = (record.meta === undefined ? '' : record.meta.slug));
+            records = records.filter(record => record.slug !== '');
+            records.map(record => record.title = record.meta.title);
+            records.map(record => record.description = record.meta.description);
+            records.map(record => record.extension = record.meta.extension);
+            records.map(record => record.createtime = record.meta.createtime);
+
+            const tagRecords = [];
+            records.forEach((record) => {
+                record.tags.forEach((tag) => {
+                    const _record = {...record};
+                    _record.tag = tag;
+                    tagRecords.push(_record);
+                });
+            });
+            Promise.all(records.map(record => ServicesIndexedDB.insertRecord(storeObject, record))).then(() => {
+                this.setState({isLoading: false});
+            });
+            Promise.all(tagRecords.map(tagRecord => ServicesIndexedDB.insertRecord(storeTagObject, tagRecord))).then(() => {
+                this.setState({isLoading: false});
+            });
+        });
+    }
+
+    init() {
+        this.initIndexedDB();
+    }
+
+    addCookie = () => {
+        const _date = new Date();
+        _date.setFullYear(_date.getFullYear() + 1);
+        document.cookie = "privacy=allow; expires=" + _date.toGMTString();
+        this.setState({privacy: true});
+    }
+
+    render() {
+        ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY);
+        ReactGA.pageview(window.location.pathname + window.location.search);
+        if (this.state.isLoading) {
+            return (
+                <div className="app__center">
+                    <span className="fas fa-spinner fa-spin"></span>
+                </div>
+            )
+        } else {
+            return (
+                <Fragment>
+                    {(!this.state.privacy) ?
+                        <div className="app__overlay">
+                            <div className="app__overlay__popup">
+                                <h2>Fontos</h2>
+                                <div className="app__overlay__popup__content">
+                                    Az oldal cookie-kat használ a jobb működés érdekében!
+                                </div>
+                                <div className="app__overlay__popup__footer">
+                                    <button onClick={this.addCookie}>Rendben</button>
+                                </div>
+                            </div>
+                        </div>
+                        : ''}
+                    <Router>
+                        <Fragment>
+                            <AppCommonHeader/>
+                            <div className="w-100 p-3">
+                                <Switch>
+                                    <Route path="/" exact component={AppFront}/>
+                                    <Route path="/notfound" exact component={AppNotfound}/>
+                                    <Route path="/search" exact component={AppSearch}/>
+                                    <Route path="/search/:slug" exact component={AppSearch}/>
+                                    <Route path="/:slug" exact component={AppTweet}/>
+                                    <Route path="/tag/:slug" exact component={AppTag}/>
+                                </Switch>
+                            </div>
+                        </Fragment>
+                    </Router>
+                </Fragment>
+            );
+        }
+    }
 }
 
 export default App;
-
 
 
 /*
--- src/common/card/Card.js
+++ src/common/card/Card.js
@@ -5,12 +5,20 @@ import {Link} from "react-router-dom";
 const AppCommonCard = (props) => {
     return (
         <div className="common__card">
+
             <Link to={`/${props.slug}`}>
+
                 <img className="common__card__image" alt={props.title}
-                     src={process.env.REACT_APP_IMAGE_URL+'/size1/' + props.slug + "." + props.extension}/>
+                     src={process.env.REACT_APP_IMAGE_URL + '/size1/' + props.slug + "." + props.extension}/>
             </Link>
             <div className="common__card__content">
-                <Link to={`/${props.slug}`} className="common__card__content__title">{props.title}</Link>
+
+                <Link to={`/${props.slug}`} className="common__card__content__title">
+                    <span className="common__card__content__title__new">NEW</span>
+                    <span>
+                    {props.title}
+                    </span>
+                </Link>
                 <div className="common__card__content__date">{props.date}</div>
                 <div className="common__tags">
                     {props.tags.map((tag) => {
@@ -20,6 +28,7 @@ const AppCommonCard = (props) => {
                     })}
                 </div>
             </div>
+
         </div>
     );
 }
